b1bbbf693d02ea0be75d50d389d5e20760e0ee5b
Kiku-stacks <brunobentosilva459@gmail.com>
2026-03-16 19:56:51 -0300

    Implement createNode function for linked list

---
+++ deque/node.c
@@ -0,0 +1,13 @@
+#include "node.h"
+#include <stdlib.h>
+
+Node* createNode(int elem, Node* ptr){
+    Node* newNode = (Node*)malloc(sizeof(Node));
+
+    if(newNode !=NULL){
+        newNode->dado = elem;
+        newNode->next = ptr;
+        newNode->prev = NULL;
+    }
+    return newNode;
+}
